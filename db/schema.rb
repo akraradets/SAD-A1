11ab9483e2d7d961893b9dfb540eb6422caa5dca
saisivap <saisivap289@gmail.com>
2019-02-08 01:49:12 +0530

    index UI

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_02_05_140110) do
 
+  create_table "children", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "newsletters", force: :cascade do |t|
     t.string "title"
     t.string "content"
